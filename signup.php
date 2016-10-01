a40da10c132a6968c6f822a704fac6369148f279
wasib <mohammedwasib@gmail.com>
2016-10-01 23:24:01 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -364,13 +364,13 @@ Animation Keyframes
 
 <section class="tech-login">
    <div id="fade-box">
- <form method="POST" class="form"  action="#">
+ 
 		    <input type="number" id="userid" name="userid" placeholder="Uid">
 			<input type="text" id="user" name="user" placeholder="Username">
 			<input type="password" id="pass" name="pass" placeholder="Password">
 			<input type="password" id="pass1" name="pass1" placeholder="Confirm Password">
 			<button type="submit" name="submit" id="signup-button">Sign-Up</button><br><span id="uname"></span><br>
-		</form>
+		
 		
 		
 		
