56e53aa2007dcc296eeeebd715a411245e4f6517
wasib <mohammedwasib@gmail.com>
2016-10-04 21:08:43 +0530

    Update help.php

--- help.php
+++ help.php
@@ -1,1 +1,14 @@
-write instructions here
+<?php
+ include 'connect.php';
+ include 'bal.php'; 
+  
+?>
+ <html>
+ <body>
+
+ <a href="home.php">back</a><br>
+   
+   write instructions here
+   
+   </body>
+</html>
