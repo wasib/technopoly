7e1b46aa9ae0b8c9f9308e23ca617198da1b16c2
wasib <mohammedwasib@gmail.com>
2016-10-04 21:04:52 +0530

    Update home.php

--- home.php
+++ home.php
@@ -6,6 +6,7 @@ include 'bal.php';
  <a href="store.php?level=1">store</a><br>
  <a href="myaccount.php">myaccount</a><br>
  <a href="market.php">market</a><br>
+ <a href="help.php">help</a><br>
  <a href="logout.php">logout</a><br>
  </body>
  </html>
